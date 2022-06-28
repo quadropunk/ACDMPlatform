eb1c83202aabfc1293dcc481a4a2ad5c769a0cf3
Arsen <abdigal.2015@gmail.com>
2022-06-28 16:52:03 +0600

    DAO fixed

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -11,62 +11,114 @@ contract DAO {
         uint256 startedTime;
         uint128 votesFor;
         uint128 votesAgainst;
+        bytes wSignature; // signature for winning case
+        bytes lSignature; // Signature for loosing case
+        address wTargetContract;
+        address lTargetContract;
     }
 
-    Counters.Counter private votingsCount;
-    uint256 private votingPeriod = 3 days;
-
-    SomeToken public token;
     mapping(uint256 => Voting) public votings;
     mapping(uint256 => mapping(address => bool)) public voters;
     mapping(address => mapping(uint256 => uint128)) public balances;
+
+    SomeToken public immutable token;
+
+    Counters.Counter private votingsCount;
+    uint256 private immutable votingPeriod = 3 days;
 
     constructor(address _token) {
         token = SomeToken(_token);
     }
 
-    function deposit(uint256 _amount) external {
-        require(_amount != 0, "DAO: Cannot deposit zero tokens");
-        token.transfer(msg.sender, _amount);
-    }
+    event VotingCreated(
+        bytes wSignature,
+        bytes lSignature,
+        address wTargetContract,
+        address lTargetContract,
+        uint256 startedTime
+    );
+    event Voted(uint128 amount, bool voteFor, uint256 indexed votingId);
+    event VotingFinished(uint256 indexed votingId, bool won);
 
-    function createVoting() external {
-        votings[votingsCount.current()] = Voting({
+    function createVoting(
+        bytes memory _wSignature,
+        bytes memory _lSignature,
+        address _wTargetContract,
+        address _lTargetContract
+    ) external {
+        Voting memory v = votings[votingsCount.current()];
+        v = Voting({
             startedTime: block.timestamp,
             votesFor: 0,
-            votesAgainst: 0
+            votesAgainst: 0,
+            wSignature: _wSignature,
+            wTargetContract: _wTargetContract,
+            lSignature: _lSignature,
+            lTargetContract: _lTargetContract
         });
+
+        emit VotingCreated(
+            v.wSignature,
+            v.lSignature,
+            v.wTargetContract,
+            v.lTargetContract,
+            v.startedTime
+        );
         votingsCount.increment();
     }
 
-    function vote(uint128 _amount, bool _voteFor, uint256 _votingId) external {
-        require(voters[_votingId][msg.sender] == false, "DAO: You have already voted");
+    function vote(
+        uint128 _amount,
+        bool _voteFor,
+        uint256 _votingId
+    ) external {
+        require(
+            voters[_votingId][msg.sender] == false,
+            "DAO: You have already voted"
+        );
         require(_amount != 0, "DAO: Cannot vote with zero tokens");
-        require(votings[_votingId].startedTime != 0, "DAO: Voting with such id does not exist");
+        require(
+            votings[_votingId].startedTime != 0,
+            "DAO: Voting with such id does not exist"
+        );
 
         token.transferFrom(msg.sender, address(this), _amount);
         balances[msg.sender][_votingId] = _amount;
         Voting storage voting = votings[_votingId];
-        if (_voteFor == true)
-            voting.votesFor += _amount;
-        else
-            voting.votesAgainst += _amount;
+        if (_voteFor == true) voting.votesFor += _amount;
+        else voting.votesAgainst += _amount;
+
+        emit Voted(_amount, _voteFor, _votingId);
     }
 
     function finishVoting(uint256 _votingId) external {
-        require(votings[_votingId].startedTime != 0, "DAO: Voting with such id does not exist");
-        require(votings[_votingId].startedTime + votingPeriod < block.timestamp, "DAO: Voting is not finished yet");
+        require(
+            votings[_votingId].startedTime != 0,
+            "DAO: Voting with such id does not exist"
+        );
+        require(
+            votings[_votingId].startedTime + votingPeriod < block.timestamp,
+            "DAO: Voting is not finished yet"
+        );
         Voting storage voting = votings[_votingId];
 
-        require(voting.votesAgainst != voting.votesFor, "DAO: 50 / 50 votes are detected");
-        bool won = voting.votesFor > voting.votesAgainst;
-        if (won == true) {
-            // Give commission to owner
-        }
-        else {
-            // Buy LPTokens for ETH on uniswap and then burn them
+        require(
+            voting.votesAgainst != voting.votesFor,
+            "DAO: 50 / 50 votes are detected"
+        );
+        if (voting.votesFor > voting.votesAgainst) {
+            (bool success, ) = voting.wTargetContract.call{value: 0}(
+                voting.wSignature
+            );
+            require(success, "ERROR call func");
+        } else {
+            (bool success, ) = voting.lTargetContract.call{value: 0}(
+                voting.lSignature
+            );
+            require(success, "ERROR call func");
         }
 
+        emit VotingFinished(_votingId, voting.votesFor > voting.votesAgainst);
         delete votings[_votingId];
     }
 }
