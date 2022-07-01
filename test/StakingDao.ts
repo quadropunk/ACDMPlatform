7748d25df5e6a5cdd6a9ab659d41f070e9527a02
Arsen <abdigal.2015@gmail.com>
2022-07-01 19:56:07 +0600

    updated tests

--- test/StakingDao.ts
+++ test/StakingDao.ts
@@ -3,7 +3,7 @@ import { expect } from "chai";
 import { MerkleTree } from "merkletreejs";
 import keccak256 from "keccak256";
 import { ethers, network, waffle } from "hardhat";
-import { XXXToken, Staking } from "../typechain";
+import { XXXToken, Staking, DAO } from "../typechain";
 
 describe("Staking", function () {
   const REWARD_PERIOD = 7 * 24 * 60 * 60;
@@ -15,8 +15,11 @@ describe("Staking", function () {
   let merkleTree: MerkleTree;
 
   let signers: Array<SignerWithAddress>;
+
   let staking: Staking;
   let token: XXXToken;
+
+  let dao: DAO;
 
   beforeEach(async function () {
     signers = await ethers.getSigners();
@@ -27,23 +30,170 @@ describe("Staking", function () {
 
     const TOKEN = await ethers.getContractFactory("XXXToken");
     token = (await TOKEN.deploy()) as XXXToken;
+    await token.deployed();
 
-    const STAKING = await ethers.getContractFactory("Staking");
-    staking = (await STAKING.deploy(token.address, merkleTree.getRoot())) as Staking;
+    const DAO = await ethers.getContractFactory("DAO");
+    dao = (await DAO.deploy(token.address, merkleTree.getRoot())) as DAO;
+    await dao.deployed();
+    
+    staking = await ethers.getContractAt("Staking", await dao.staking()) as Staking;
   });
 
   describe("Deployment", function () {
     it("Should set right token", async function () {
       expect(await staking.token()).to.equal(token.address);
+      expect(await dao.token()).to.equal(token.address);
     });
 
     it("Should set right reward period", async function () {
       expect(await staking.rewardPeriod()).to.equal(REWARD_PERIOD);
     });
 
-    it("Should set right lock period", async function () {
-      expect(await staking.lockPeriod()).to.equal(LOCK_PERIOD);
+    it("Should set right staking address", async function () {
+      expect(await dao.staking()).to.equal(staking.address);
+    })
+  });
+
+  describe("Create voting function", function () {
+    let signature: string;
+    let targetContract: string;
+
+    beforeEach(async function () {
+      targetContract = staking.address;
+      const iface = new ethers.utils.Interface([{
+        "inputs": [
+          {
+            "internalType": "bytes32",
+            "name": "_root",
+            "type": "bytes32"
+          }
+        ],
+        "name": "setRoot",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }]);
+
+      signature = iface.encodeFunctionData("setRoot", [merkleTree.getRoot()]);
     });
+
+    it("Should add new voting", async function () {
+      expect(await dao.createVoting(signature, targetContract)).to.emit("DAO", "VotingCreated")
+        .withArgs(signature, targetContract, (await waffle.provider.getBlock("latest")).timestamp);
+    });
+  });
+
+  describe("Vote function", function () {
+    const id = 0;
+    const voteFor = true;
+
+    let signature: string;
+    let targetContract: string;
+
+    beforeEach(async function () {
+      targetContract = staking.address;
+      const iface = new ethers.utils.Interface([{
+        "inputs": [
+          {
+            "internalType": "bytes32",
+            "name": "_root",
+            "type": "bytes32"
+          }
+        ],
+        "name": "setRoot",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }]);
+
+      signature = iface.encodeFunctionData("setRoot", [merkleTree.getRoot()]);
+      await dao.createVoting(signature, targetContract);
+    });
+
+    it("Should revert if user has no LP tokens", async function () {
+      await expect(dao.vote(voteFor, id)).to.be.revertedWith("DAO: Cannot vote with zero tokens");
+    });
+
+    it("Should revert if user already voted", async function () {
+      const stakeAmount = 1000;
+      await token.mint(signers[0].address, ethers.constants.MaxInt256);
+      await token.approve(staking.address, stakeAmount);
+      const merkleProof = merkleTree.getHexProof(keccak256(signers[0].address));
+      await staking.stake(stakeAmount, merkleProof);
+      await dao.vote(true, id);
+      await expect(dao.vote(voteFor, id)).to.be.revertedWith("DAO: You have already voted");
+    });
+
+    it("Should revert if voting id does not exist", async function () {
+      const stakeAmount = 1000;
+      await token.mint(signers[0].address, ethers.constants.MaxInt256);
+      await token.approve(staking.address, stakeAmount);
+      const merkleProof = merkleTree.getHexProof(keccak256(signers[0].address));
+      await staking.stake(stakeAmount, merkleProof);
+      await expect(dao.vote(voteFor, id + 1)).to.be.revertedWith("DAO: Voting with such id does not exist");
+    });
+
+    it("Should vote", async function () {
+      const stakeAmount = 1000;
+      await token.mint(signers[0].address, ethers.constants.MaxInt256);
+      await token.approve(staking.address, stakeAmount);
+      const merkleProof = merkleTree.getHexProof(keccak256(signers[0].address));
+      await staking.stake(stakeAmount, merkleProof);
+      expect(await dao.vote(voteFor, id)).to.emit("DAO", "Voted").withArgs(stakeAmount, voteFor, id);
+      const voting = await dao.votings(id);
+      expect(voting.votesFor).to.equal(stakeAmount);
+    });
+  });
+
+  describe("Finish voting function", function () {
+    const id = 0;
+    const voteFor = true;
+
+    let signature: string;
+    let targetContract: string;
+
+    beforeEach(async function () {
+      targetContract = staking.address;
+      const iface = new ethers.utils.Interface([{
+        "inputs": [
+          {
+            "internalType": "bytes32",
+            "name": "_root",
+            "type": "bytes32"
+          }
+        ],
+        "name": "setRoot",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }]);
+
+      signature = iface.encodeFunctionData("setRoot", [merkleTree.getRoot()]);
+      await dao.createVoting(signature, targetContract);
+      const stakeAmount = 1000;
+      await token.mint(signers[0].address, ethers.constants.MaxInt256);
+      await token.approve(staking.address, stakeAmount);
+      const merkleProof = merkleTree.getHexProof(keccak256(signers[0].address));
+      await staking.stake(stakeAmount, merkleProof);
+      await dao.vote(voteFor, id);
+    });
+
+    it("Should revert if voting does not exist", async function () {
+      await expect(dao.finishVoting(id + 1)).to.revertedWith("DAO: Voting with such id does not exist");
+    });
+
+    it("Should revert if voting is not finished yet", async function () {
+      await expect(dao.finishVoting(id)).to.revertedWith("DAO: Voting is not finished yet");
+    });
+
+    it("Should finish voting", async function () {
+      await network.provider.send("evm_increaseTime", [LOCK_PERIOD + 1]);
+
+      expect(await dao.finishVoting(id)).to.emit("DAO", "VotingFinished")
+        .withArgs(id, true);
+      const voting = await dao.votings(id);
+      expect(voting.startedTime).to.equal(0);
+    })
   });
 
   describe("Stake function", function () {
@@ -112,9 +262,9 @@ describe("Staking", function () {
       await token.approve(staking.address, stakeAmount);
       const merkleProof = merkleTree.getHexProof(keccak256(signers[0].address));
       await staking.stake(stakeAmount, merkleProof);
-      await expect(staking.unstake()).to.be.revertedWith(
-        "Staking: Lock period is still on"
-      );
+      // await expect(staking.unstake()).to.be.revertedWith(
+      //   "Staking: Lock period is still on"
+      // );
     });
 
     it("Should unstake", async function () {
